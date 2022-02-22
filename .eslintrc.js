7e9c00d9a936c8a0f0eff4daa576ab42b084fc2b
Alison Vortmann <alisonvort@gmail.com>
2022-02-21 22:46:56 -0300

    chore: create and update of some config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["airbnb-base"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
+    quotes: "double",
   },
 };
