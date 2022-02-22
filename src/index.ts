cb8d9fbbfed8ec6582dc3f6a3480c0295b8b5661
Alison Vortmann <alisonvort@gmail.com>
2022-02-21 22:53:38 -0300

    feat: add the index file to execute the application

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import path from "path";
+import { Plateau } from "./controllers/Plateau";
+import { Rover } from "./controllers/Rover";
+import { ReadFile } from "./services/readFile";
+
+//Retrieve data from the input file
+const directoryPath = path.join(__dirname.replace("src", "src/input"));
+const input = ReadFile(directoryPath);
+
+//Separates plateau limits from rover commands
+const plateau_limits = input
+  .splice(0, 1)
+  .toString()
+  .split(" ")
+  .map((item) => {
+    return item;
+  });
+
+//Organizes the all the rovers and it's inputs
+const rovers = [];
+for (let index = 0; index < input.length; index += 2) {
+  rovers.push({
+    first_coordinates: input[index].toString(),
+    instructions: input[index + 1].toString(),
+  });
+}
+
+//Checks if all the limits are given and creates the Plateau object
+let plateau;
+if (
+  plateau_limits &&
+  plateau_limits[0] &&
+  plateau_limits[1] &&
+  plateau_limits.length === 2
+) {
+  plateau = new Plateau({
+    x_coordinate: parseInt(plateau_limits[0], 10),
+    y_coordinate: parseInt(plateau_limits[1], 10),
+  });
+} else {
+  throw new Error(
+    "The Plateau limits were not defined correctly, check the instructions"
+  );
+}
+
+//Place the rovers on the Plateau
+const rover = new Rover();
+console.log(rovers);
+const response = rover.execute(plateau, rovers);
+console.log(response);
